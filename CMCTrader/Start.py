4c61dcd33c62374780227e53e839bf01d5f3e04b
Ethan <ethanjohol@gmail.com>
2018-11-08 17:10:42 +1100

    minor fixes

--- CMCTrader/Start.py
+++ CMCTrader/Start.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import selenium.webdriver.support.ui as ui
 from selenium.common.exceptions import StaleElementReferenceException
 
